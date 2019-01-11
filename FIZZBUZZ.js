68fca51b9a8107138b0d4d89595076e0ca650397
tdude0175 <tdude0175@gmail.com>
2019-01-11 11:54:42 -0600

    Completed optional work.

--- FIZZBUZZ.js
+++ FIZZBUZZ.js
@@ -6,3 +6,32 @@
 // If the current number is evenly divisible by 5 you should log BUZZ and the number
 // If the current number is evenly divisible by both 3 and 5 you should log FIZZBUZZ and the number
 // Otherwise, just print the current number
+
+
+var num1 = parseInt(prompt("enter a number."));
+
+for(var numstr = 1;numstr <=num1; numstr++)
+{
+    //var Divis_3 = (parseFloat(numstr)/3);
+    var Divis_3 = numstr%3;
+    var Divis_5 = numstr%5;
+
+    if(0 === Divis_3 && Divis_5 === 0 )
+    {
+        console.log("FIZZBUZZ");
+    }
+    else if(Divis_5 === 0 )
+    {
+        console.log("BUZZ");
+    }
+    else if (Divis_3 === 0)
+    {
+        console.log("FIZZ");
+    }
+    else
+        {
+            console.log(numstr);
+        }
+}
+
+// trying to use parseFloat and parseInt to confirm wether a number is divisble by a number.
--- ex5.js
+++ ex5.js
@@ -7,21 +7,24 @@
 // Calculates the monthly payment they need to make and logs it to the console as To pay of a loan of LOANAMT it will take NUMMONTHS monthly payments.
 // BONUS: Keep prompting until the User actually enters a number for both loan amount and number of months (HINT: Look into using NaN)
 
-
-var LOANAMT = prompt(" Enter loan amount required.");
-var NUMMONTHS = prompt(" How many months will you have to pay it off?");
-
-var PAYMONTHS = parseInt(LOANAMT / NUMMONTHS);
-
-if(LOANAMT === NaN || NUMMONTHS === NaN)
-{
-    console.log("Please input amount in numbers.");
-}
-else
+do
     {
-        console.log("It will take " + NUMMONTHS + " monthly payments of " + PAYMONTHS + " to pay back " + LOANAMT + " amount of funds.");
-    }
 
+    var LOANAMT = parseFloat(prompt(" Enter loan amount required."));
+    var NUMMONTHS = parseFloat(prompt(" How many months will you have to pay it off?"));
+
+    var PAYMONTHS = parseInt(LOANAMT / NUMMONTHS);
+
+    if(isNaN(LOANAMT)|| isNaN(NUMMONTHS))
+    {
+        console.log("Please input amount in numbers.");
+
+    }
+    else
+        {
+            console.log("It will take " + NUMMONTHS + " monthly payments of " + PAYMONTHS + " to pay back " + LOANAMT + " amount of funds.");
+        }
+    }while(isNaN(LOANAMT) && isNaN(NUMMONTHS));
 //how to get console to pick up on not a number?
 // parseInt used to make code more preety,
 // Without parseInt will return repeating decimals, same can be said for parseFloat.
